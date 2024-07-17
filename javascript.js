c548250d2cd060be6952372ede52363899d7f94a
Vishal H. Chauhan <vishalc@sfs.net>
2024-07-17 09:53:37 +0530

    game completed with score update

--- javascript.js
+++ javascript.js
@@ -6,6 +6,8 @@ const button = document.querySelector(".scissors")
 button.parentNode.insertBefore(div, button.nextSibling)
 const div2 = document.createElement("div")
 button.parentNode.insertBefore(div2, button.nextSibling)
+const div3 = document.createElement("div")
+button.parentNode.insertBefore(div3, button.nextSibling)
 
 
 function getComputerChoice() {
@@ -21,62 +23,79 @@ function getComputerChoice() {
     }
 }
 
-function playround(computerChoice, humanChoice) {
-    if (humanscore >= 5) {
+function finalResult(humanscore, computerscore){
+    if (humanscore >= 5 ) {
         div2.textContent = (`You Have Won!! Game is Over. Your Score: ${humanscore} & Computer Score: ${computerscore}`);
     }
-    else if (computerscore >= 5) {
+    else if (computerscore >=5) {
         div2.textContent = (`Game is Over. Your Score: ${humanscore} & Computer Score: ${computerscore}`);
     }
-    else {
+}
+
+function playround(computerChoice, humanChoice) {
         if (computerChoice === "rock" && humanChoice.toLowerCase() === "paper") {
             div.textContent = ('You Won! Paper beats Rock')
             console.log("You Won! Paper beats Rock")
             humanscore = humanscore + 1
+            div3.textContent = (`Your Score: ${humanscore} & Computer Score: ${computerscore}`);
+            finalResult(humanscore, computerscore)
         }
         else if (computerChoice === "paper" && humanChoice.toLowerCase() === "rock") {
             div.textContent = ('You lose! Paper beats Rock')
             console.log("You lose! Paper beats Rock")
             computerscore = computerscore + 1
+            div3.textContent = (`Your Score: ${humanscore} & Computer Score: ${computerscore}`);
+            finalResult(humanscore, computerscore)
+
         }
         else if (computerChoice === "rock" && humanChoice.toLowerCase() === "scissors") {
             div.textContent = ('You lose! Rock beats Scissors')
             console.log("You lose! Rock beats Scissors")
             computerscore = computerscore + 1
+            div3.textContent = (`Your Score: ${humanscore} & Computer Score: ${computerscore}`);
+            finalResult(humanscore, computerscore)
+
         }
         else if (computerChoice === "scissors" && humanChoice.toLowerCase() === "rock") {
             div.textContent = ('You Won! Rock beats Scissors')
             console.log("You Won! Rock beats Scissors")
             humanscore = humanscore + 1
+            div3.textContent = (`Your Score: ${humanscore} & Computer Score: ${computerscore}`);
+            finalResult(humanscore, computerscore)
+
         }
         else if (computerChoice === "scissors" && humanChoice.toLowerCase() === "paper") {
             div.textContent = ('You lose! Scissors beats Paper')
             console.log("You lose! Scissors beats Paper")
             computerscore = computerscore + 1
+            div3.textContent = (`Your Score: ${humanscore} & Computer Score: ${computerscore}`);
+            finalResult(humanscore, computerscore)
+
         }
         else if (computerChoice === "paper" && humanChoice.toLowerCase() === "scissors") {
             div.textContent = ('You Won! Scissors beats Paper')
             console.log("You Won! Scissors beats Paper")
             humanscore = humanscore + 1
+            div3.textContent = (`Your Score: ${humanscore} & Computer Score: ${computerscore}`);
+            finalResult(humanscore, computerscore)
+
         }
         else {
             console.log("It's a Draw!")
             div.textContent = (`It's a Draw!`)
-
-        }
+            div3.textContent = (`Your Score: ${humanscore} & Computer Score: ${computerscore}`);
+            finalResult(humanscore, computerscore)
     }
 
 }
 
-
 function playGame() {
+    div3.textContent = (`Your Score: ${humanscore} & Computer Score: ${computerscore}`);
     console.log("Final Score Computer:", computerscore, "Final Score Human:",)
 }
 
 
 playGame()
-
-div2.textContent = computerscore
 
 const rock = document.querySelector('.rock')
 rock.addEventListener('click', () => {
